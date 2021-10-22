944a070a82712a2716cf9177fc12c40a735bea3e
trigaten <sanderman5@hotmail.com>
2021-10-22 13:41:57 -0400

    Oh windows

---
+++ training/models.py
@@ -0,0 +1,8 @@
+import torch
+import torch.nn as nn
+
+class seq2seq(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.encoder = nn.GRU(30, 1200, 3, batch_first=True, bidirectional=True)
+        self.decoder = nn.GRU(30, 1200, 3, batch_first=True, bidirectional=True)
