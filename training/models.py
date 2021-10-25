8da4fc7c3900f561d7d503dd59abeff1d3e6adf8
trigaten <sanderman5@hotmail.com>
2021-10-25 19:23:01 -0400

    various fixes

--- training/models.py
+++ training/models.py
@@ -6,6 +6,8 @@ class seq2seq(nn.Module):
         super().__init__()
         self.encoder = nn.GRU(30, 1200, 3, batch_first=True, bidirectional=True)
         self.decoder = nn.GRU(30, 1200, 3, batch_first=True, bidirectional=True)
-    
+        
     def forward(x):
         return 0
+
+# if __name__ == '__main__':
