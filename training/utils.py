f35e650559fb22ebab4b91a3e461f1c47a1b474f
trigaten <sanderman5@hotmail.com>
2021-11-03 17:32:17 -0400

    embeddings and preprocessing utils

--- training/utils.py
+++ training/utils.py
@@ -41,15 +41,48 @@ def preprocess(df, binary_classes=True):
     
     # make all lowercase
     df[pp_cols] = df[pp_cols].applymap(lambda s:s.lower())
-
-    # tokenize data
-    df[pp_cols] = df[pp_cols].applymap(lambda s:word_tokenize(s))    
-
+    
     # combine title and body of post
-    df["post_body"] = df["title"] + df["post_body"]
+    df["post_body"] = df["post_body"].applymap(lambda x: x + " ")
+    df["post_body"] = df["title"] + " " +  df["post_body"]
 
     # get rid of now redundant title column
     df.drop(columns=["title"], axis=1, inplace=True)
+
+    # apply to post only
+
+    # Remove all the special characters
+    df["post_body"] = df["post_body"].applymap(lambda s:re.sub(r'\W', ' ',s))
+
+    # remove all single characters
+    df["post_body"] = df["post_body"].applymap(lambda s:re.sub(r'\s+[a-zA-Z]\s+', ' ',s))
+
+    # Remove single characters from the start (no space before them, but space after)
+    df["post_body"] = df["post_body"].applymap(lambda s:re.sub(r'^[a-zA-Z]\s+', ' ',s))
+
+    # Removing prefixed 'b'
+    df["post_body"] = df["post_body"].applymap(lambda s:re.sub(r'^b\s+', ' ',s,flags=re.I))
+
+    # remove all of: x200b (zero-width space)
+    df["post_body"] = df["post_body"].applymap(lambda s:re.sub(r'x200b', ' ',s))
+
+    # Substituting multiple spaces with single space
+    df["post_body"] = df["post_body"].applymap(lambda s:re.sub(r'\s+', ' ',s,flags=re.I))
+
+    # Remove single numbers
+    df["post_body"] = df["post_body"].applymap(lambda s:re.sub(r'\d+', ' ',s,flags=re.I))
+
+    # remove stopwords
+    df["post_body"] = df["post_body"].applymap(lambda s: [word for word in s if word not in stopwords.words('english')])
+    
+    # tokenize df
+    df["post_body"] = df["post_body"].applymap(lambda s:word_tokenize(s))
+
+    # perform stemming
+    df["post_body"] = df["post_body"].applymap(lambda s: [ps.stem(word) for word in s])
+
+     # tokenize data
+    df[pp_cols] = df[pp_cols].applymap(lambda s:word_tokenize(s))    
 
     if binary_classes:
         df["judgement"] = df["judgement"].map({"YTA":1, "ESH":1, "NTA":0, "NAH":0})
@@ -126,6 +159,7 @@ def get_embeddings(path="embeddings_30_1000", special_tokens=True):
         gensim_embeds.wv[START_TOKEN] = np.random.normal(0, 1, size=(gensim_embeds.vector_size))
         gensim_embeds.wv[END_TOKEN] = np.random.normal(0, 1, size=(gensim_embeds.vector_size))
         gensim_embeds.wv[UNKNOWN_TOKEN] = np.random.normal(0, 1, size=(gensim_embeds.vector_size))
+    
     return gensim_to_pytorch(gensim_embeds)
     
 if __name__ == "__main__":
